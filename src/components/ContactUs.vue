4b93b8b046a27c2f844c3f34ad9ddbfd5c0ffc28
hayashi-3 <hayashi.gml@gmail.com>
2021-05-05 12:20:52 +0900

    [add]sucsess_page

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,43 +1,9 @@
 <template>
-  <div class="p-contact">
-    <form class="p-contact__form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
-      <input type="hidden" name="form-name" value="contact">
-
-      <div class="p-contact__item">
-        <label for="username">お名前</label>
-        <input type="text" id="username" name="username" v-model="username" autocomplete="name">
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item">
-        <label for="katakana">フリガナ</label>
-        <input type="text" id="katakana" name="katakana" v-model="katakana">
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item">
-        <label for="useremail">メールアドレス</label>
-        <input type="text" id="useremail" name="useremail" v-model="useremail" autocomplete="email">
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item">
-        <label for="message">お問い合わせ内容</label>
-        <textarea id="message" name="message" v-model="message"></textarea>
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item" v-show="false">
-        <label for="message">スパムでない場合は空欄</label>
-        <input type="text" name="bot-field" v-model="botField"/>
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__submit">
-        <button type="submit">送信</button>
-      </div>
-      <!-- /.p-contact__submit -->
-    </form>
-    <!-- /.p-contact__form -->
-  </div>
+  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+    <input type="hidden" name="form-name" value="contact" />
+    <input id="name" name="name" type="text" />
+    <input id="email" name="email" type="email"/>
+    <textarea id="message" name="message" />
+    <button type="submit">送信</button>
+  </form>
 </template>
