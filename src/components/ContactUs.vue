364d1bd6558e83a1e8d326464c4adcef20b15e38
hayashi-3 <hayashi.gml@gmail.com>
2021-05-07 22:00:51 +0900

    [test]form

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,81 +1,73 @@
 <template>
-   <v-container tag="section">
-    <v-row>
-      <v-col
-        v-text="title.toUpperCase()"
-        cols="12"
-        tag="h1"
-      />
-    </v-row>
-    <v-form method="post" netlify>
-      <v-text-field
-        v-show="false"
-        v-model="title"
-        name="form-name"
-      />
-      <v-row>
-        <v-col cols="12">
+  <v-form name="contact" method="POST" netlify>
+    <v-text-field
+    v-show="false"
+    v-model="title"
+    name="form-name"
+    />
+    <input type="hidden" name="form-name" value="contact" />
+    <v-container>
+      <h2>お問い合わせフォーム</h2>
+        <v-col
+          cols="12"
+          sm="6"
+        >
           <v-text-field
             v-model="name"
-            label="name"
+            label="お名前"
             name="name"
-            autofocus
-          />
+            clearable
+          ></v-text-field>
         </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-            name="email"
-          />
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+        <v-text-field
+          v-model="email"
+          label="email"
+          name="email"
+          clearable
+        ></v-text-field>
         </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-textarea
-            v-model="message"
-            label="message"
-            name="message"
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn
-            :disabled="isEmpty"
-            type="submit"
-          >
-            submit
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
-  </v-container>
+
+      <v-textarea
+          outlined
+          v-model="message"
+          label="お問い合わせ"
+          name="message"
+          clearable
+      ></v-textarea>
+
+      <v-btn type="submit" color="primary" @click="submit">送信</v-btn>
+
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      title: 'contact',
-      name: '',
-      email: '',
-      message: '' 
+      name: "",
+      email: "",
+      message: "",
     }
   },
-  computed: {
-    isEmpty () {
-      if (
-        this.name !== '' &&
-        this.email !== '' &&
-        this.message !=='' 
-      ) {
-        return false
-      }
-      return true
-    }
-  }
+  methods: {
+    async submit() {
+      const params = new FormData()
+      //以下、ダミーフォームの各フォーム要素のnameと合わせる
+      params.append('form-name', 'contact')
+      params.append('name', this.name)
+      params.append('email', this.email)
+      params.append('message', this.message)
+
+      const response = await this.$axios.$post(window.location.origin, params)
+      //実際はresponseを使って画面側にフィードバックさせるが、ここでは仮にconsoleに出力
+      console.log(response)
+    },
+  },
 }
 </script>
